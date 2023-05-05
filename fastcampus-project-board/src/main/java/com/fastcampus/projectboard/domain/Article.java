6225fee2266c561763f8e331da8c62d2e0ba3d85
JayeunPark <jayeunpark0704@gmail.com>
2023-05-05 18:16:54 +0900

    #11-article.java파일 수정
    블록을 닫지 않아 오류가 생겨 수정함.

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -29,10 +29,15 @@ public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Setter @Column(nullable = false) private String title; // 제목
-    @Setter @Column(nullable = false,length = 10000) private String content; // 본문
+    @Setter
+    @Column(nullable = false)
+    private String title; // 제목
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content; // 본문
 
-    @Setter private String hashtag; // 해시태그
+    @Setter
+    private String hashtag; // 해시태그
 
     @ToString.Exclude
     @OrderBy("id")
@@ -40,35 +45,45 @@ public class Article {
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
     //metadata
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false,length = 100) private String createdBy; //생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false,length = 100) private String modifiedBy; //수정자
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt; // 생성일시
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    private String createdBy; //생성자
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modifiedBy; //수정자
 
 
-    protected Article(){}
+    protected Article() {
+    }
 
-    public Article(String title, String content, String hashtag){
+    public Article(String title, String content, String hashtag) {
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
-//of 메소드 : 주어진 매개변수를 사용하여 새로운 Article 객체를 생성하고 반환
-    public static Article of(String title, String content, String hashtag){
-        return new Article(title,content,hashtag);
+
+    //of 메소드 : 주어진 매개변수를 사용하여 새로운 Article 객체를 생성하고 반환
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
     }
 
-//객체의 동등성 비교와 해시 코드 계산
+    //객체의 동등성 비교와 해시 코드 계산
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Article)) return false;
         Article article = (Article) o;
-        return id!=null&&id.equals(article.id);
+        return id != null && id.equals(article.id);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
-
+}
